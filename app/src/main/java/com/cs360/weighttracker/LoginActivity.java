f02f09d671dc3a3832cc801e9ac9d2a2c2499cb4
Alan Abraham Puthenparambil Kochumon <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2026-07-26 13:59:53 +0530

    feat: add basic setup to login activity

--- app/src/main/java/com/cs360/weighttracker/LoginActivity.java
+++ app/src/main/java/com/cs360/weighttracker/LoginActivity.java
@@ -1,6 +1,11 @@
 package com.cs360.weighttracker;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 
@@ -8,11 +13,42 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
+
+    EditText usernameEditText, passwordEditText;
+    Button loginButton, registerButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
+
+        setupUI();
+        setupEvents();
     }
+
+    private void setupUI() {
+        usernameEditText = findViewById(R.id.etLoginUsername);
+        passwordEditText = findViewById(R.id.etLoginPassword);
+        loginButton = findViewById(R.id.btnLogin);
+        registerButton = findViewById(R.id.btnCreateAccount);
+    }
+
+
+    private void setupEvents() {
+        loginButton.setOnClickListener(view -> {
+            Log.d("TEST", "Login button clicked!");
+        });
+        registerButton.setOnClickListener(view ->{
+            Log.d("TEST", "Register button clicked!");
+        });
+    }
+
+    /// Establish flow
+    // Register
+    // Login
+    // Username validation
+    // Password validation
+    // setup ui
 
 }
